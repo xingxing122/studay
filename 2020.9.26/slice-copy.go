9dca1d7b537a6fca9eeae854d63cef2b19eb6995
Xingxing <xingxingwang122@gmail.com>
2020-11-13 17:00:33 +0800

    Default Changelist 切片操作

---
+++ 2020.9.26/slice-copy.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(len(nums), cap(nums))
+	nums = append(nums, 6)
+	fmt.Println(len(nums), cap(nums))
+	nums = append(nums, 7)
+	fmt.Println(len(nums), cap(nums))
+
+}
