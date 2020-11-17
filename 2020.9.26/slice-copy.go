376bd7cadcac530223bffaa77e7f0a32ca52c611
Xingxing <xingxingwang122@gmail.com>
2020-11-17 18:24:33 +0800

    slice-copy

--- 2020.9.26/slice-copy.go
+++ 2020.9.26/slice-copy.go
@@ -3,15 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5}
-	nums2 := nums[1:3]
+    // 复制nums 中所有数据到nums2
+	 //第一种方法
+	//nums := []int{1,2,3,4,5}
+	 //nums2 := []{}
+	 //copy(nums2,nums)
 
-	nums = append(nums, 1000)
-	nums2 = append(nums2, 100)
+	 /* 第二种方法
+	nums  := []int{1,2,3,4,5}
+	 nums2 := make([]int,len(nums))
+	 for i, v := range nums {
+	 	nums2[i] =v
+	 }
+     fmt.Println(nums2)*/
 
+	nums := []int{1,2,3,4,5}
+	nums2 := make ([]int,0,len(nums))
+	for _,v := range nums {
+	nums2 = append(nums2,v)
+	}
 	fmt.Println(nums2)
-	fmt.Println(nums)
-	fmt.Println(cap(nums2))
-    fmt.Println(cap(nums))
 
 }
