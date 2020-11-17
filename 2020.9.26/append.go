84778297b47af5c61664bae9664e1d693f988c7d
xingxing <xingxingwang122@gmail.com>
2020-11-18 07:53:38 +0800

    切片操作

--- 2020.9.26/append.go
+++ 2020.9.26/append.go
@@ -7,10 +7,18 @@ func main() {
 	nums2 :=[]int{3,4,5}
 
 
-	nums = append(nums,100)
-	for _, v:=range nums2 {
+	nums = append(nums,100,102,105)
+	fmt.Println(nums,nums2)
+
+	/*for _, v:=range nums2 {
 		nums = append(nums,v)
 	}
       fmt.Println(nums,nums2)
+	*/
+    // 切片解包
+	nums = append(nums,nums2...)
+	fmt.Println(nums,nums2)
+
+
 
 }
---
+++ 2020.9.26/slice-apple.go
@@ -0,0 +1,8 @@
+package main
+
+
+func main(){
+	queue :=[]int{}
+
+
+}
