ef18206da2d8b18de3dba916066d87cbd9e54f75
Xingxing <xingxingwang122@gmail.com>
2020-11-24 18:27:02 +0800

    string

--- 2020.9.26/string-01.go
+++ 2020.9.26/string-01.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    var  name   string = "a\\nb"
+    var  name   string = `xingxing`
     fmt.Printf("%T,%s\n",name,name)
 
-
-
+    name += "hi"
+    fmt.Println(name)
+    name = "我爱中国"
+    //索引
+    fmt.Printf("%T\n",name[0])
+    //长度
+    fmt.Println(len(name))
+    //切片
+    fmt.Println(name[0:4])
 
 }
