eab6fe4a5cb84ae8f377d0c9c550bf5bcf6c14b8
xingxing <xingxingwang122@gmail.com>
2020-12-18 08:48:36 +0800

    mp

--- 2020.9.26/mp.go
+++ 2020.9.26/mp.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	heights := []int{10, 6, 7, 9, 5}
 	for j := 0; j < len(heights)-1; j++ {
-		fmt.Println("第",j,"轮")
+		fmt.Println("第", j, "轮")
 		for i := 0; i < len(heights)-1; i++ {
 			if heights[i] > heights[i+1] {
 				fmt.Println("交换:", heights[i])
