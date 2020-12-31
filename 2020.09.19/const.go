4bc54b07abf714abe7cd82db2ba613198cc06309
Xingxing <xingxingwang122@gmail.com>
2020-12-31 20:06:50 +0800

    golang

---
+++ 2020.09.19/const.go
@@ -0,0 +1,16 @@
+package main
+
+import  "fmt"
+
+// 定义常量(需要初始化值)
+const     statusNew  int  = 1
+const   statusDeleted  int  =  2
+func main() {
+      const  (
+      	Monday = 10
+      	Tuesday
+	  )
+     fmt.Println(statusNew,statusDeleted)
+      fmt.Println(Monday,Tuesday)
+	
+}
---
+++ 2020.09.19/debug.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a = 1
+    fmt.Println(a)
+
+    a = a + 3
+    fmt.Println(a)
+
+}
