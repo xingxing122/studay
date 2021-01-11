5bbef9f68d968a1e6dbd81e931dcc2f598c04633
Xingxing <xingxingwang122@gmail.com>
2021-01-11 18:13:03 +0800

    golang

---
+++ 2020.09.26/slicememory.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1,2,3,4,5}
+	fmt.Println(len(nums),cap(nums))
+	nums = append(nums,6)
+	fmt.Println(len(nums),cap(nums))
+    nums= append(nums,7)
+    fmt.Println(len(nums),cap(nums))
+    nums = append(nums,8)
+    fmt.Println(len(nums),cap(nums))
+
+}
