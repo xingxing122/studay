fc7a29a70a73eb6a6960391d2f2fadeb29a88168
Xingxing <xingxingwang122@gmail.com>
2021-01-19 18:23:20 +0800

    golang

--- 2020.09.26/mapv1.go
+++ 2020.09.26/mapv1.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+)
 
 func main() {
-    m := map[int]string{1:"mike",2: "yoyo", 3:"go"}
-    for key,  value := range  m {
-    	fmt.Printf("%d=====> s%\n",key,value)
+	var name map[string]string = map[string]string{"Go3007":"中国"}
+	fmt.Printf("%T %v\n",name,name)
+	fmt.Printf("%q\n",name["G036"])
 
-	}
+	name["Go3006"]="学习go"
+	fmt.Println(name)
+
+	//判断key 是否存在
+	 v,ok  := name["Go0002"]
+	 fmt.Println(v,ok)
+
+	 for k,v :=range  name {
+	 	fmt.Println(v,k)
+	 }
+
+	 var score = make(map[string]int)
+	 score["Go3028"]  = 90
+	 score["Go3018"]  = 91
+	 score["Go3008"]  = 92
+	 score["Go3098"]  = 92
+
+	 for k,v :=range  score {
+	 	fmt.Println(k,v)
+	 }
+
+
+
 
 
 
--- 2020.09.26/statv2.go
+++ 2020.09.26/statv2.go
@@ -13,16 +13,13 @@ func main() {
 		"小黑",
 		"小红",
 		"大黄",
+		"大黄",
 	}
-
-	stat  := map[string]int{}
-	for _,name := range name {
-		stat[name]++
+   //	var  stat map[string]int = map[string]int{}
+   //	fmt.Println(stat)
+    stat  := map[string]int{}
+   	for  _,name := range  name {
+             stat[name]++
 	}
 	fmt.Println(stat)
-
-
-
-
-
 }
