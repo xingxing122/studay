1baf29ea29a6cb3fe74490fe3a06fb0d0f932616
Xingxing <xingxingwang122@gmail.com>
2021-01-25 18:29:06 +0800

    golang

--- 2020.09.26/map.go
+++ 2020.09.26/map.go
@@ -3,49 +3,41 @@ package  main
 import "fmt"
 
 func main(){
-	// 字面量初始化
-	var name map[string]string = map[string]string{"go3037":"李雄"}
+	   //var name map[string]string
+	   var  name map[string]string  = map[string]string{"G0001":"我爱祖国"}
+	   fmt.Printf("%T  %v\n",name,name)
 
-	//map[keytype]valuetype{k:v1,k:v2}
+	   fmt.Printf("%q\n",name["Go3005"])
+	   name["Go3005"] ="我爱中国"
+	   fmt.Println(name)
+	   name["Go3002"]="我爱家乡"
+	   fmt.Println(name)
 
+	   v, ok := name["Go00001"]
+	   fmt.Println(v,ok)
 
-	fmt.Printf("%T %v\n",name,name)
-	name["Go3038"] ="王星星"
-	fmt.Println(name)
+	   delete(name,"Go3002")
+	   fmt.Println(name)
 
-	v, ok := name["Go0001"]
-	fmt.Println(v,ok)
+       for k := range  name {
+       	fmt.Println(k)
+	   }
 
-	v, ok = name["Go3038"]
-	fmt.Println(v,ok)
+	   for v,k := range name {
+	   	fmt.Println(v,k)
+	   }
 
-	delete(name,"Go3038")
-	fmt.Println(name)
+      var  int = make(map[string]int)
+      int ["Go3002"] = 90
+      int ["Go3003"] = 91
+      int ["Go30004"] = 92
 
-	fmt.Println("_____________________")
+      for k,v := range int {
+      	fmt.Println(k,v)
+	  }
+	  for v := range  int {
+	  	fmt.Println(v)
+	  }
 
-	for k := range name {
-		fmt.Println(k,name[k])
-	}
-
-	for v ,k := range  name {
-		fmt.Println(v,k)
-	}
-
-
-	var score = make(map[string]int)
-	score["01"] = 90
-	score["02"] = 80
-	score["03"] = 87
-	score["04"] = 79
-	score["05"] = 78
-	score["06"] = 76
-	score["07"] = 75
-	score["08"] = 60
-	fmt.Println(score)
-
-	for v,k := range score{
-		fmt.Println(v,k)
-	}
 
 }
