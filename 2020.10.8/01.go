d887a2cb621a3fd893be0d9a3594187c5e62ff4a
Xingxing <xingxingwang122@gmail.com>
2021-02-18 17:56:28 +0800

    go

--- 2020.10.8/01.go
+++ 2020.10.8/01.go
@@ -2,16 +2,53 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 )
 
-func main() {
-	who := "World"
-	if len(os.Args) > 1 {
-		who = strings.Join(os.Args[1:],"")
-
-	}
-	fmt.Println("Hello World!",who)
+func  sayHi(name string){
+	fmt.Println("hi",name)
 
 }
+
+func add(a int, b int) int {
+	return a + b //】 // 参数中多个连续的参数类型相同
+
+}
+
+func  addV2(a,b int) int{
+	return  a + b
+}
+
+func addAll(a,b int,args ... int)int{
+	fmt.Println(a,b,args)
+	fmt.Printf("%T\n",args)
+	sum := a + b
+	for _,v := range  args {
+		sum +=v
+	}
+	return sum
+}
+
+func print (args ... int){
+	for i,v := range args {
+		fmt.Println(i,v)
+	}
+}
+
+
+func main(){
+	sayHi("KK")
+	name := "我爱我家"
+	sayHi(name)
+	c := add(2,3)
+	fmt.Println(c)
+	fmt.Println(addV2(3,4))
+	fmt.Println(addAll(1,2))
+	fmt.Println(addAll(1,2,3,4))
+
+	nums :=[]int{1,2,3,4,5}
+	fmt.Println(addAll(1,2,nums...))
+	print(nums...)
+
+
+}
+
--- 2020.10.8/02.go
+++ 2020.10.8/02.go
@@ -1,12 +1,44 @@
 package main
 
-import "fmt"
+import (
+      "fmt"
+)
+
+func  sayHello(){
+      fmt.Println("hello")
+}
+
+func add(a,b int) int{
+      return a + b
+}
+
+//返回多个值
+func op(a,b int)(int,int,int,int){
+      return  a + b, a - b , a * b, a/b
+}
+
+// 命名返回值
+func opv2(a,b int)(sum int, sub int,mul int, div int){
+      sum  = a + b
+      sub  = a - b
+      mul  = a * b
+      div  = a /b
+      return
+}
+
 
 func main(){
-      s :=[]string{"A","B","C","D","E","F","G"}
-      s =s[2:]
-      fmt.Println(s)
+      sayHello()
+      add(1,2)
+      c := add(3,4)
+      fmt.Println(c)
+      fmt.Println(op(4,2))
+      a,b,c,d :=op(8,2)
+      fmt.Println("a=",a,"b=",b,"c=",c,"d=",d)
 
+      fmt.Println(opv2(3,2))
 
 }
+
+
 
