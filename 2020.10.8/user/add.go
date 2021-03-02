5dfa5e804615b4b81ee5a68f980315508f8d0c5a
Xingxing <xingxingwang122@gmail.com>
2021-03-02 18:21:51 +0800

    go

---
+++ 2020.10.8/user/add.go
@@ -0,0 +1,19 @@
+package main
+
+//定义存储用户的切片
+//每个元素为一个用户
+//var users []map[string]string{} =[]map[string]string{}
+
+var   user  = []map[string] string {}
+
+func getID() string{
+	id := 0
+	for _,user := range users {
+		user["id"]
+	}
+}
+
+func main() {
+
+
+}
