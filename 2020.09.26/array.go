ab7fcc982138779f127b7415ec7779fe178638b2
Xingxing <xingxingwang122@gmail.com>
2021-05-11 18:20:20 +0800

    go

--- 2020.09.26/array.go
+++ 2020.09.26/array.go
@@ -1,45 +1,40 @@
-package main
+package  main
 
 import "fmt"
 
-func  main(){
-	//定义变量names 为元素类型
-	var  names [55] string
-	var  scores = [...]int{3:100,1:88,0:99}
+func   main(){
+	var names [55]string
 
-	fmt.Printf("%T\n",names)
+	var  scores = [...]int{10000,90}
+	fmt.Printf("%T\n",names )
 	fmt.Printf("%T\n",scores)
+	fmt.Println(names,scores)
 
-    fmt.Println(names,scores)
+	var  nums[2]int = [...]int{10000,90}
 
+	fmt.Println(scores == nums)
 
-	var  nums [2] int =[...] int {100,88}
-	fmt.Printf("%T\n",names)
-	fmt.Printf("%q\n",names)
-	fmt.Printf("%T\n",scores)
-
-	fmt.Println(nums)
-	nums[1] = 10000
 	fmt.Println(nums[0])
 	fmt.Println(nums[1])
+	nums[0]=101
+	fmt.Println(nums)
+	nums[1]=103
+	fmt.Println(nums)
+
 	fmt.Println(len(nums))
 	fmt.Println(cap(nums))
 
-
-	for i := 0; i < len(nums);i++ {
+	for i:=0;i< len(nums);i++{
 		fmt.Println(i,nums[i])
+
+	}
+	for  v := range nums {
+		fmt.Println(v,nums[v])
 	}
 
-	for v:= range nums {
-		fmt.Println(nums[v])
-	}
 
-    for  i, v := range  nums{
-    	fmt.Println(i,v)
-	}
 
 
 
 
 }
-
