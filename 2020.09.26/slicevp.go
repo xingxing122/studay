91854e915f33d4cfefaacdf48106e7ea57aa16ad
Xingxing <xingxingwang122@gmail.com>
2021-07-23 17:31:46 +0800

    go

--- 2020.09.26/slicevp.go
+++ 2020.09.26/slicevp.go
@@ -8,5 +8,15 @@ func main() {
     fmt.Println(nums[:3])
     fmt.Println(nums[3:len(nums)])
 
-    
+    nums2 := []int{3,4,5}
+
+    /*for _,v := range nums2 {
+        nums = append(nums,v)
+    }
+     fmt.Println(nums,nums2)
+      */
+
+    nums = append(nums,nums2...)
+    fmt.Println(nums,nums2)
+
 }
