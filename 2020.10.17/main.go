ea8dd0f9d3b68437c856abbe558db031bf00269b
Xingxing <xingxingwang122@gmail.com>
2021-08-09 18:08:39 +0800

    cka

--- 2020.10.17/main.go
+++ 2020.10.17/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-study/2020.10.17/math"
+)
 
-func  main(){
 
-	fmt.Println("hello world!!!")
-
+func main(){
+	fmt.Println(math.Add(1,2))
 }
--- 2020.10.17/math/math.go
+++ 2020.10.17/math/math.go
@@ -1,4 +1,4 @@
-package main
+package math
 
 func   Add(n1,n2 int)int{
 	return  n1 + n2
