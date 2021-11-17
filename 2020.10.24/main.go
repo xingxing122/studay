a50f39585d72ae3d4972ea8e7c3447474ba4fc53
Xingxing <xingxingwang122@gmail.com>
2021-11-17 18:14:59 +0800

    blog

--- 2020.10.24/main.go
+++ 2020.10.24/main.go
@@ -1,16 +1,21 @@
 package main
 
+import  "fmt"
+
+// 现有的数据类型
 type  Counter int
 
-
 func main() {
+	var counter Counter
+	fmt.Printf("%T,%v\n", counter,counter)
+	counter += 10
+	fmt.Printf("%T,%v\n",counter,counter)
+	var num int = 10
+	c2 := Counter(num)+counter
 
+	fmt.Printf("%T, %v\n",c2,c2)
 
-
-
-
-
-
-
+   // var  user  User
+	// fmt.Printf("%T,%v\n",user,user)
 
 }
