65c75748e3c02b30321208b12008acda76a09d76
wangxingxing <wangxingxing@yushu.biz>
2022-08-30 17:55:23 +0800

    go

---
+++ 变量与常量/01.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+//
+/* 全局变量*/
+var name string = "xingxing"
+func main(){
+
+var (
+age int =31 
+weight int = 170
+) 
+
+height, weight := 179, 199 
+
+fmt.Println(name,age,weight,height)
+
+}
---
+++ 变量与常量/assign.go
@@ -0,0 +1,14 @@
+package main  
+
+import "fmt"
+func main() {
+  var (
+	  age = 30
+	  weight  = 138 
+  )
+
+  fmt.Println(age,weight)
+
+
+
+}
