67276eff1ae67f0b23bb1e0e993270e5a10cf398
wangxingxing <wangxingxing@yushu.biz>
2024-10-08 18:36:38 +0800

    go

---
+++ basic_grammar/01.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var s1 string = "你好拥抱go"
+	fmt.Println(s1)
+
+	var a int
+	var b float32
+	var c float64
+	var e string
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(e)
+
+}
---
+++ basic_grammar/02.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n1 int = 100
+	fmt.Println(n1)
+
+	var n2 float32 = float32(n1)
+	fmt.Println(n2)
+	//注意，N1 的类型还是int 类型，只是将N1的值100 转为了float32而已，N1 还是int 类型
+
+	fmt.Printf("%T", n1)
+
+	var n3 int64 = 88888
+	var n4 int8 = int8(n3)
+	fmt.Println(n4)
+
+	var n5 int32 = 12
+	var n6 int64 = int64(n5) + 30
+	fmt.Println(n5)
+	fmt.Println(n6)
+}
