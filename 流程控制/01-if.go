b031f521043d14f70291ced7e2d73daf66af933b
xingxingwang <xingxingwang122@gmail.com>
2025-03-10 22:59:03 +0800

    go

---
+++ 流程控制/01-if.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 01
+	age := 30
+	if age > 20 {
+		fmt.Println("成年人")
+	}
+
+	//02
+
+	if a := 34; a > 30 {
+		fmt.Println("成年人", a)
+	}
+	//03
+	// var score = 75
+	//	if score >= 90 {
+	//		fmt.Println("A")
+	//	} else if score > 75 {
+	//		fmt.Println("B")
+	//	} else {
+	//		fmt.Println("C")
+	//	}
+
+}
