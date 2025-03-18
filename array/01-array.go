9e22bae225856e985a6518595c48e4ccb541c95b
wangxingxing <wangxingxing@yushu.biz>
2025-03-18 18:14:49 +0800

    go for

--- array/01-array.go
+++ array/01-array.go
@@ -34,13 +34,23 @@ func main() {
 		fmt.Println(sum)
 		fmt.Printf("arr数组的和是: %v  平均值是： %.2f", sum, float64(sum)/float64(len(arr)))*/
 
-	var arr = [...]int{1, -10, 100, 96, 209}
-	max := arr[0]
-	for _, k := range arr {
-		if max < k {
-			max = k
+	/*	var arr = [...]int{1, -10, 100, 96, 209}
+		max := arr[0]
+		for _, k := range arr {
+			if max < k {
+				max = k
+			}
+		}
+		fmt.Printf("max: %d\n", max)
+	*/
+
+	//从数组 [1,3,5,7,8] 中找出和为8 的两个元素的下标分别为(0，3)和（1,2）
+	var arr = [...]int{1, 3, 5, 7, 8}
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i]+arr[j] == 8 {
+				fmt.Printf("(%v ,%v)", i, j)
+			}
 		}
 	}
-	fmt.Printf("max: %d\n", max)
-
 }
