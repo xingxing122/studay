3a9b23636976295d69661fb25b21a99f4dbba74e
wangxingxing <wangxingxing@yushu.biz>
2025-04-18 17:29:04 +0800

    func

--- function/demo1.go
+++ function/demo1.go
@@ -1,31 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func cal(num1 int, num2 int) int {
-
-	var sum int = 0
-	sum += num1
-	sum += num2
+// 求两个数的和
+func sumFN(x int, y int) int {
+	sum := x + y
 	return sum
 }
 
-func cal12(num1 int, num2 int) (int, int) {
+// 求两个数的差
+func subFn(x int, y int) int {
+	sub := x - y
+	return sub
 
-	var sum int = 0
-	sum += num1
-	sum += num2
+}
 
-	var result int = num1 - num2
-	return sum, result
+func sumFn1(x ...int) int {
+
+	/*	fmt.Printf("%v--%T", x, x)*/
+	sum := 0
+	for _, v := range x {
+
+		sum += v
+	}
+	return sum
+}
+
+func calc(x, y int) (int, int) {
+	sum := x + y
+	sub := x - y
+	return sum, sub
+
 }
 
 func main() {
-	// 调用 cal 并将返回值赋给 sum
-	//sum := cal(10, 20)
-	//fmt.Println(sum) // 打印结果
-	sum1, _ := cal12(10, 20)
-	// fmt.Println(num1)
+	sum1 := sumFN(12, 3)
 	fmt.Println(sum1)
-	
+
+	a := 20
+	b := 2
+	sub1 := subFn(a, b)
+	fmt.Println(sub1)
+
+	sum3 := sumFn1(12, 34, 45, 46)
+	fmt.Println(sum3)
+
+	a1, b1 := calc(100, 200)
+	fmt.Println(a1, b1)
+
 }
