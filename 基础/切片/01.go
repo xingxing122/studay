42285b7342466e9817f6d3e913ba8c09080b52a9
xingxingwang <xingxingwang122@gmail.com>
2025-09-07 22:31:34 +0800

    go

--- 基础/切片/01.go
+++ 基础/切片/01.go
@@ -1,1 +1,16 @@
-package 切片
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num := []int{1, 2, 3, 4}
+
+	strs := []string{"张三", "lisi"}
+
+	fmt.Println(num, strs)
+
+	fmt.Printf("长度: %v, 容量: %v", len(num), cap(num))
+
+}
